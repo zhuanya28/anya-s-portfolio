af66f0a6aab498f5dc9c463c01595499708bbad7
zhuanya28 <azhukova1402@gmail.com>
2023-04-08 18:07:13 +0800

    fixed: arrow & expansion

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,23 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
+import { faAnglesDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Project(props) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleClick = () => {
+    setIsExpanded(!isExpanded);
+  };
 
   return (
-    <div className="projects main">
-      <h1 className="projectName">{props.name}</h1>
-      <h3 className="projectDate">{props.date}</h3>
-      <h5 className="projectRole">{props.role}</h5>
-      <h5 className="projectMedium">{props.medium}</h5>
-      <img className="projectsImages" src={props.media} alt={props.media} />
-      <p className="projectDescription">{props.description}</p>
-      <a
-        className="projectLink"
-        href={props.link}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {props.link}
-      </a>
+    <div
+      className={` box ${isExpanded ? "expandedProject" : "closedProject"}`}
+      onClick={handleClick}
+    >
+      <div className="projects">
+        <FontAwesomeIcon
+          icon={faAnglesDown}
+          className={`arrow ${isExpanded ? "rotate" : ""}`}
+        />
+        <h1 className="projectName">{props.name}</h1>
+        <h3 className="projectDate">{props.date}</h3>
+        <h5 className="projectRole">{props.role}</h5>
+        {isExpanded && (
+          <>
+            <h5 className="projectMedium">{props.medium}</h5>
+            <img
+              className="projectsImages"
+              src={props.media}
+              alt={props.media}
+            />
+            <p className="projectDescription">{props.description}</p>
+            <a
+              className="projectLink"
+              href={props.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {props.link}
+            </a>
+          </>
+        )}
+      </div>
     </div>
   );
 }
