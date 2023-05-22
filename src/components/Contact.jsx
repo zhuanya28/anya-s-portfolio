33f56d80c38a089392365e488abc779ace8fcc04
zhuanya28 <azhukova1402@gmail.com>
2023-05-22 11:19:57 +0800

    fixed: contact and menu dissappears when the screen size is smaller than 1000px

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,60 +9,49 @@ function Contact() {
     <div className="contactPage">
       <div className="contactPageIcon">
         <a
+          className="contactPageIcon"
           target="_blank"
           rel="noopener noreferrer"
           href="https://www.linkedin.com/in/zhuanya/"
         >
-          <svg>
-            <image
-              className="contactIcon"
-              xlinkHref={linkedIn}
-            
-            />
+          <svg className="contactIcon">
+            <image className="contactIcon" xlinkHref={linkedIn} />
           </svg>
         </a>
       </div>
       <div className="contactPageIcon">
         <a
+          className="contactPageIcon"
           target="_blank"
           rel="noopener noreferrer"
           href="mailto:zhuanya@nyu.edu"
         >
-          <svg>
-            <image
-              className="contactIcon"
-              xlinkHref={email}
-          
-            />
+          <svg className="contactIcon">
+            <image className="contactIcon" xlinkHref={email} />
           </svg>
         </a>
       </div>
       <div className="contactPageIcon">
         <a
+          className="contactPageIcon"
           target="_blank"
           rel="noopener noreferrer"
           href="https://t.me/zhuanya14"
         >
-          <svg>
-            <image
-              className="contactIcon"
-              xlinkHref={telegram}
-              
-            />
+          <svg className="contactIcon">
+            <image className="contactIcon" xlinkHref={telegram} />
           </svg>
         </a>
       </div>
       <div className="contactPageIcon">
         <a
+          className="contactPageIcon"
           target="_blank"
           rel="noopener noreferrer"
           href="https://drive.google.com/file/d/11S9MyukDqzKQrzuQyfdxfIB1ePn0wm1s/view?usp=sharing"
         >
-          <svg>
-            <image
-              className="contactIcon"
-              xlinkHref={wechat}
-            />
+          <svg className="contactIcon">
+            <image className="contactIcon" xlinkHref={wechat} />
           </svg>
         </a>
       </div>
